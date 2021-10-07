7c49d2676510fdd6a98d140362cf92b71a939abf
Jedidiah <jedidiah.hwang.dev@gmail.com>
2021-10-07 14:10:21 -0600

    added css endpoint

--- server/server.js
+++ server/server.js
@@ -7,7 +7,13 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/index.html"));
 });
 
+app.get("/styles.css", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/styles.css"));
+});
+
 app.use("/css", express.static(path.join(__dirname, "../client/styles.css")));
+
+
 
 const port = process.env.PORT || 4005;
 
