e4ab8240965f118ca01b87b0d8deb177b5393dcb
Jedidiah <jedidiah.hwang.dev@gmail.com>
2021-10-07 14:13:28 -0600

    last test, removed middleware

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
 //     res.sendFile(path.join(__dirname, "../client/styles.css"));
 // });
 
-app.use("/styles.css", express.static(path.join(__dirname, "../client/styles.css")));
+// app.use("/styles.css", express.static(path.join(__dirname, "../client/styles.css")));
 
 
 
