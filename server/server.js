466130233f9b735f088fdc0996d3bce8e174c529
Jedidiah <jedidiah.hwang.dev@gmail.com>
2021-10-07 14:55:26 -0600

    endpoint functionality added again

--- server/server.js
+++ server/server.js
@@ -7,9 +7,9 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/index.html"));
 });
 
-// app.get("/styles.css", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../client/styles.css"));
-// });
+app.get("/styles.css", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/styles.css"));
+});
 
 // app.use("/styles.css", express.static(path.join(__dirname, "../client/styles.css")));
 
